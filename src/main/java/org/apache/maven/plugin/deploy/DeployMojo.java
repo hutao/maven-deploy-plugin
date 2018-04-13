e0c99f9622a17d13dfe5cc7f06ff1b3804b167d3
胡涛 <hutao@yingyinglicai.com>
2018-04-13 18:10:22 +0800

    升级功能

--- src/main/java/org/apache/maven/plugin/deploy/DeployMojo.java
+++ src/main/java/org/apache/maven/plugin/deploy/DeployMojo.java
@@ -1,5 +1,24 @@
 package org.apache.maven.plugin.deploy;
 
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.ArtifactUtils;
+import org.apache.maven.artifact.deployer.ArtifactDeploymentException;
+import org.apache.maven.artifact.metadata.ArtifactMetadata;
+import org.apache.maven.artifact.repository.ArtifactRepository;
+import org.apache.maven.artifact.repository.layout.ArtifactRepositoryLayout;
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Writer;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.project.artifact.ProjectArtifactMetadata;
+import org.codehaus.plexus.util.FileUtils;
+import org.codehaus.plexus.util.IOUtil;
+import org.codehaus.plexus.util.WriterFactory;
+
 import java.io.File;
 import java.io.IOException;
 import java.io.Writer;
@@ -31,27 +50,9 @@ import java.util.regex.Pattern;
  * under the License.
  */
 
-import org.apache.maven.artifact.Artifact;
-import org.apache.maven.artifact.ArtifactUtils;
-import org.apache.maven.artifact.deployer.ArtifactDeploymentException;
-import org.apache.maven.artifact.metadata.ArtifactMetadata;
-import org.apache.maven.artifact.repository.ArtifactRepository;
-import org.apache.maven.artifact.repository.layout.ArtifactRepositoryLayout;
-import org.apache.maven.model.Model;
-import org.apache.maven.model.io.xpp3.MavenXpp3Writer;
-import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.plugins.annotations.LifecyclePhase;
-import org.apache.maven.plugins.annotations.Mojo;
-import org.apache.maven.plugins.annotations.Parameter;
-import org.apache.maven.project.MavenProject;
-import org.apache.maven.project.artifact.ProjectArtifactMetadata;
-import org.codehaus.plexus.util.IOUtil;
-import org.codehaus.plexus.util.WriterFactory;
-
 /**
  * Deploys an artifact to remote repository.
- * 
+ *
  * @author <a href="mailto:evenisse@apache.org">Emmanuel Venisse</a>
  * @author <a href="mailto:jdcasey@apache.org">John Casey (refactoring only)</a>
  * @version $Id: DeployMojo.java 1620080 2014-08-23 21:34:57Z khmarbaise $
@@ -59,57 +60,57 @@ import org.codehaus.plexus.util.WriterFactory;
 @Mojo(name = "deploy", defaultPhase = LifecyclePhase.DEPLOY, threadSafe = true)
 public class DeployMojo extends AbstractDeployMojo {
 
-    private static final Pattern             ALT_REPO_SYNTAX_PATTERN = Pattern
-        .compile("(.+)::(.+)::(.+)");
+    private static final Pattern ALT_REPO_SYNTAX_PATTERN = Pattern
+            .compile("(.+)::(.+)::(.+)");
 
     /**
      * When building with multiple threads, reaching the last project doesn't
      * have to mean that all projects are ready to be deployed
      */
-    private static final AtomicInteger       readyProjectsCounter    = new AtomicInteger();
+    private static final AtomicInteger readyProjectsCounter = new AtomicInteger();
 
-    private static final List<DeployRequest> deployRequests          = Collections
-        .synchronizedList(new ArrayList<DeployRequest>());
+    private static final List<DeployRequest> deployRequests = Collections
+            .synchronizedList(new ArrayList<DeployRequest>());
 
     /**
      */
     @Parameter(defaultValue = "${project}", readonly = true, required = true)
-    private MavenProject                     project;
+    private MavenProject project;
 
     @Parameter(defaultValue = "${reactorProjects}", required = true, readonly = true)
-    private List<MavenProject>               reactorProjects;
+    private List<MavenProject> reactorProjects;
 
     /**
      * Whether every project should be deployed during its own deploy-phase or
      * at the end of the multimodule build. If set to {@code true} and the build
      * fails, none of the reactor projects is deployed.
      * <strong>(experimental)</strong>
-     * 
+     *
      * @since 2.8
      */
     @Parameter(defaultValue = "false", property = "deployAtEnd")
-    private boolean                          deployAtEnd;
+    private boolean deployAtEnd;
 
     /**
      * @deprecated either use project.getArtifact() or
-     *             reactorProjects.get(i).getArtifact()
+     * reactorProjects.get(i).getArtifact()
      */
     @Parameter(defaultValue = "${project.artifact}", required = true, readonly = true)
-    private Artifact                         artifact;
+    private Artifact artifact;
 
     /**
      * @deprecated either use project.getPackaging() or
-     *             reactorProjects.get(i).getPackaging()
+     * reactorProjects.get(i).getPackaging()
      */
     @Parameter(defaultValue = "${project.packaging}", required = true, readonly = true)
-    private String                           packaging;
+    private String packaging;
 
     /**
      * @deprecated either use project.getFile() or
-     *             reactorProjects.get(i).getFile()
+     * reactorProjects.get(i).getFile()
      */
     @Parameter(defaultValue = "${project.file}", required = true, readonly = true)
-    private File                             pomFile;
+    private File pomFile;
 
     /**
      * Specifies an alternative repository to which the project artifacts should
@@ -129,60 +130,65 @@ public class DeployMojo extends AbstractDeployMojo {
      * </dl>
      */
     @Parameter(property = "altDeploymentRepository")
-    private String                           altDeploymentRepository;
+    private String altDeploymentRepository;
 
     /**
      * The alternative repository to use when the project has a snapshot
      * version.
-     * 
-     * @since 2.8
+     *
      * @see DeployMojo#altDeploymentRepository
+     * @since 2.8
      */
     @Parameter(property = "altSnapshotDeploymentRepository")
-    private String                           altSnapshotDeploymentRepository;
+    private String altSnapshotDeploymentRepository;
 
     /**
      * The alternative repository to use when the project has a final version.
-     * 
-     * @since 2.8
+     *
      * @see DeployMojo#altDeploymentRepository
+     * @since 2.8
      */
     @Parameter(property = "altReleaseDeploymentRepository")
-    private String                           altReleaseDeploymentRepository;
+    private String altReleaseDeploymentRepository;
 
     /**
      * @deprecated either use project.getAttachedArtifacts() or
-     *             reactorProjects.get(i).getAttachedArtifacts()
+     * reactorProjects.get(i).getAttachedArtifacts()
      */
     @Parameter(defaultValue = "${project.attachedArtifacts}", required = true, readonly = true)
-    private List                             attachedArtifacts;
+    private List attachedArtifacts;
 
     /**
      * Set this to 'true' to bypass artifact deploy
-     * 
+     *
      * @since 2.4
      */
     @Parameter(property = "maven.deploy.skip", defaultValue = "false")
-    private boolean                          skip;
+    private boolean skip;
 
     public void execute() throws MojoExecutionException, MojoFailureException {
         boolean addedDeployRequest = false;
+
+        boolean isFacade = (project.getArtifactId().contains("facade") && project.hasParent())
+                || ("pom".equals(project.getPackaging()) && !project.hasParent());
         if (skip) {
             getLog().info("Skipping artifact deployment");
-        } else {
+        } else if (isFacade) {
             failIfOffline();
             DeployRequest currentExecutionDeployRequest = new DeployRequest().setProject(project)
-                .setUpdateReleaseInfo(isUpdateReleaseInfo())
-                .setRetryFailedDeploymentCount(getRetryFailedDeploymentCount())
-                .setAltReleaseDeploymentRepository(altReleaseDeploymentRepository)
-                .setAltSnapshotDeploymentRepository(altSnapshotDeploymentRepository)
-                .setAltDeploymentRepository(altDeploymentRepository);
+                    .setUpdateReleaseInfo(isUpdateReleaseInfo())
+                    .setRetryFailedDeploymentCount(getRetryFailedDeploymentCount())
+                    .setAltReleaseDeploymentRepository(altReleaseDeploymentRepository)
+                    .setAltSnapshotDeploymentRepository(altSnapshotDeploymentRepository)
+                    .setAltDeploymentRepository(altDeploymentRepository);
             if (!deployAtEnd) {
                 deployProject(currentExecutionDeployRequest);
             } else {
                 deployRequests.add(currentExecutionDeployRequest);
                 addedDeployRequest = true;
             }
+        } else {
+            getLog().info("略过非facade包");
         }
 
         boolean projectsReady = readyProjectsCounter.incrementAndGet() == reactorProjects.size();
@@ -193,25 +199,30 @@ public class DeployMojo extends AbstractDeployMojo {
                 }
             }
         } else if (addedDeployRequest) {
-            getLog().info("Deploying " + project.getGroupId() + ":" + project.getArtifactId() + ":"
-                          + project.getVersion() + " at end");
+            getLog().info(
+                    "Deploying " + project.getGroupId() + ":" + project.getArtifactId() + ":"
+                            + project.getVersion() + " at end");
         }
     }
 
     private void deployProject(DeployRequest request) throws MojoExecutionException,
-                                                      MojoFailureException {
+            MojoFailureException {
 
         Artifact artifact = request.getProject().getArtifact();
         String packaging = request.getProject().getPackaging();
-        // File pomFile = request.getProject().getFile();
-        File pomFile = generatePomFile(request.getProject());
+        File pomFile;
+        if ("jar".equals(request.getProject().getPackaging())) {
+            pomFile = generatePomFile(request.getProject());
+        } else {
+            pomFile = request.getProject().getFile();
+        }
 
         @SuppressWarnings("unchecked")
         List<Artifact> attachedArtifacts = request.getProject().getAttachedArtifacts();
 
         ArtifactRepository repo = getDeploymentRepository(request.getProject(),
-            request.getAltDeploymentRepository(), request.getAltReleaseDeploymentRepository(),
-            request.getAltSnapshotDeploymentRepository());
+                request.getAltDeploymentRepository(), request.getAltReleaseDeploymentRepository(),
+                request.getAltSnapshotDeploymentRepository());
 
         String protocol = repo.getProtocol();
 
@@ -243,7 +254,6 @@ public class DeployMojo extends AbstractDeployMojo {
         }
 
         int retryFailedDeploymentCount = request.getRetryFailedDeploymentCount();
-
         try {
             if (isPomArtifact) {
                 deploy(pomFile, artifact, repo, getLocalRepository(), retryFailedDeploymentCount);
@@ -254,17 +264,17 @@ public class DeployMojo extends AbstractDeployMojo {
                     deploy(file, artifact, repo, getLocalRepository(), retryFailedDeploymentCount);
                 } else if (!attachedArtifacts.isEmpty()) {
                     getLog().info(
-                        "No primary artifact to deploy, deploying attached artifacts instead.");
+                            "No primary artifact to deploy, deploying attached artifacts instead.");
 
                     Artifact pomArtifact = artifactFactory.createProjectArtifact(
-                        artifact.getGroupId(), artifact.getArtifactId(), artifact.getBaseVersion());
+                            artifact.getGroupId(), artifact.getArtifactId(), artifact.getBaseVersion());
                     pomArtifact.setFile(pomFile);
                     if (request.isUpdateReleaseInfo()) {
                         pomArtifact.setRelease(true);
                     }
 
                     deploy(pomFile, pomArtifact, repo, getLocalRepository(),
-                        retryFailedDeploymentCount);
+                            retryFailedDeploymentCount);
 
                     // propagate the timestamped version to the main artifact
                     // for the attached artifacts to pick it up
@@ -273,29 +283,46 @@ public class DeployMojo extends AbstractDeployMojo {
                     String message = "The packaging for this project did not assign a file to the build artifact";
                     throw new MojoExecutionException(message);
                 }
-            }
 
-            for (Artifact attached : attachedArtifacts) {
-                deploy(attached.getFile(), attached, repo, getLocalRepository(),
-                    retryFailedDeploymentCount);
+                for (Artifact attached : attachedArtifacts) {
+                    deploy(attached.getFile(), attached, repo, getLocalRepository(), retryFailedDeploymentCount);
+                }
             }
         } catch (ArtifactDeploymentException e) {
             throw new MojoExecutionException(e.getMessage(), e);
+        } finally {
+            StringBuilder builder = new StringBuilder();
+            builder.append(getLocalRepository().getBasedir());
+            builder.append("/");
+            builder.append(artifact.getGroupId().replaceAll("[.]", "/"));
+            builder.append("/");
+            builder.append(artifact.getArtifactId());
+            builder.append("/");
+            builder.append(artifact.getBaseVersion());
+
+            getLog().info("清空本地 intall 的目录." + builder.toString());
+            try {
+                FileUtils.cleanDirectory(new File(builder.toString()));
+            } catch (IOException e) {
+                getLog().warn(e.getMessage());
+            }
         }
     }
 
-    ArtifactRepository getDeploymentRepository(MavenProject project, String altDeploymentRepository,
+    ArtifactRepository getDeploymentRepository(MavenProject project,
+                                               String altDeploymentRepository,
                                                String altReleaseDeploymentRepository,
-                                               String altSnapshotDeploymentRepository) throws MojoExecutionException,
-                                                                                       MojoFailureException {
+                                               String altSnapshotDeploymentRepository)
+            throws MojoExecutionException,
+            MojoFailureException {
         ArtifactRepository repo = null;
 
         String altDeploymentRepo;
         if (ArtifactUtils.isSnapshot(project.getVersion())
-            && altSnapshotDeploymentRepository != null) {
+                && altSnapshotDeploymentRepository != null) {
             altDeploymentRepo = altSnapshotDeploymentRepository;
         } else if (!ArtifactUtils.isSnapshot(project.getVersion())
-                   && altReleaseDeploymentRepository != null) {
+                && altReleaseDeploymentRepository != null) {
             altDeploymentRepo = altReleaseDeploymentRepository;
         } else {
             altDeploymentRepo = altDeploymentRepository;
@@ -308,7 +335,7 @@ public class DeployMojo extends AbstractDeployMojo {
 
             if (!matcher.matches()) {
                 throw new MojoFailureException(altDeploymentRepo, "Invalid syntax for repository.",
-                    "Invalid syntax for alternative repository. Use \"id::layout::url\".");
+                        "Invalid syntax for alternative repository. Use \"id::layout::url\".");
             } else {
                 String id = matcher.group(1).trim();
                 String layout = matcher.group(2).trim();
@@ -317,7 +344,7 @@ public class DeployMojo extends AbstractDeployMojo {
                 ArtifactRepositoryLayout repoLayout = getLayout(layout);
 
                 repo = repositoryFactory.createDeploymentArtifactRepository(id, url, repoLayout,
-                    true);
+                        true);
             }
         }
 
@@ -327,7 +354,7 @@ public class DeployMojo extends AbstractDeployMojo {
 
         if (repo == null) {
             String msg = "Deployment failed: repository element was not specified in the POM inside"
-                         + " distributionManagement element or in -DaltDeploymentRepository=id::layout::url parameter";
+                    + " distributionManagement element or in -DaltDeploymentRepository=id::layout::url parameter";
 
             throw new MojoExecutionException(msg);
         }
@@ -337,10 +364,9 @@ public class DeployMojo extends AbstractDeployMojo {
 
     /**
      * Generates a minimal POM from the user-supplied artifact information.
-     * 
+     *
      * @return The path to the generated POM file, never <code>null</code>.
-     * @throws MojoExecutionException
-     *             If the generation failed.
+     * @throws MojoExecutionException If the generation failed.
      */
     private File generatePomFile(MavenProject project) throws MojoExecutionException {
         Model model = generateModel(project);
@@ -356,7 +382,7 @@ public class DeployMojo extends AbstractDeployMojo {
             return tempFile;
         } catch (IOException e) {
             throw new MojoExecutionException("Error writing temporary pom file: " + e.getMessage(),
-                e);
+                    e);
         } finally {
             IOUtil.close(fw);
         }
@@ -364,7 +390,7 @@ public class DeployMojo extends AbstractDeployMojo {
 
     /**
      * Generates a minimal model from the user-supplied artifact information.
-     * 
+     *
      * @return The generated model, never <code>null</code>.
      */
     @SuppressWarnings("unchecked")
@@ -376,7 +402,6 @@ public class DeployMojo extends AbstractDeployMojo {
         model.setVersion(project.getVersion());
         model.setPackaging(project.getPackaging());
         model.setDescription(project.getDescription());
-        model.setDependencies(project.getDependencies());
         model.setPackaging(project.getPackaging());
         model.setDistributionManagement(project.getDistributionManagement());
         model.setProperties(project.getProperties());
